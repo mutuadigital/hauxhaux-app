e8fb283e7f668441a8e840cc5d2f3e83fb858280
HauxHaux Deploy <caminhardonovomundo@gmail.com>
2026-04-17 18:58:41 -0300

    fix: alterar porta padrao para 3001

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = process.env.HOSTNAME || 'localhost';
-const port = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT || '3001', 10);
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
